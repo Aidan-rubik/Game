1e192bbc379608e605f8c9b697225e1c59b7bc4c
Aidan-rubik <vikiaidanreneo@gmail.com>
2025-08-18 15:11:44 +0700

    Add files via upload

---
+++ clickergame.js
@@ -0,0 +1,31 @@
+const skor = document.getElementById("jumlahSkor");
+
+let count = 0;
+
+document.getElementById("game").onclick = function() {
+    count++;
+    skor.textContent = count;
+}
+
+//document.getElementById("powerup").onclick = function() {
+//    if(skor >= 10){
+//        skor.textContent = skor - 10;
+//    }
+//    else{
+//        window.alert("Skor Kamu Belum Cukup!");
+//    }
+//
+//}
+//    <button id="powerup"><strong>Skor+<br>Level: <label id="level">0</label><br>Harga: 10</strong></button>
+//#powerup{
+//    color: aliceblue;
+//    background-color: rgb(14, 12, 160);
+//    font-size: 15px;
+//    margin: 50px;
+//    padding: 20px 40px;
+//    cursor: pointer;
+//    transition: background-color 0.25s;
+//}
+//#powerup:hover{
+//    background-color: blue;
+//}
